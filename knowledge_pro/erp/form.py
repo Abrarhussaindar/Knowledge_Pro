46bd581d8f47df689fbdb262b9d9b4d1bb221ea0
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2021-10-26 18:44:32 +0530

    first commit

---
+++ knowledge_pro/erp/form.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.db.models import fields
+from .models import *
+from django.contrib.auth.forms import UserCreationForm
+
+class CreateCustomer(UserCreationForm):
+    class Meta:
+        model = Student
+        fields = ('first_name', 'middle_name', 'last_name', 'phone_number' , 'email', 'password1', 'password2')
+
+class TakeProfilePic(forms.ModelForm):
+    class Meta:
+        model = Profile_pic
+        fields = ('image', )
+
+class hostel_leave_form(forms.ModelForm):
+    class Meta:
+        model = Hostel_Leave_Entry
+        fields = ('academic_year', 'roll_number', 'out_pass_type', 'out_pass_from', 'out_pass_to' , 'request_type', 'reason')
+
+
+class request_form(forms.ModelForm):
+    class Meta:
+        model = Support_Request_form
+        fields = ('roll_number', 'category_name', 'description')
---
+++ knowledge_pro/erp/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.home_page, name="home"),
+    path('', views.login_page, name="login"),
+
+
+    path('attendance/', views.attendance, name="attendance"),
+    path('absence details/', views.absence_details, name="absence details"),
+    path('previous attendance/', views.previous_attendance, name="previous attendance"),
+    path('time table/', views.time_table, name="time table"),
+    path('result/', views.result, name="result"),
+    path('previous result/', views.previous_result, name="previous result"),
+    path('view course plan/', views.view_course_plan, name="view course plan"),
+    # path('download course plan/<int:id>/', views.download_course_plan, name="download course plan"),
+    path('fee payment/', views.fee_payment, name="fee payment"),
+    path('fee payment history/', views.fee_payment_history, name="fee payment history"),
+    path('hostel leave entry/', views.hostel_leave_entry, name="hostel leave entry"),
+    path('hostel leave entry form/', views.hostel_leave_entry_form, name="hostel leave entry form"),
+    path('support/', views.support, name="support"),
+    path('support request form/', views.support_request_form, name="support request form"),
+    # path('course registration/', views.course_registration, name="course registration"),
+
+]
