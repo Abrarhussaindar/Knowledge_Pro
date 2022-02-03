c84668b13a10e184bcab6b39e5d45e83595e1894
Abrar hussain dar <63048954+Abrarhussaindar@users.noreply.github.com>
2022-02-03 21:23:27 +0530

    uploading

--- knowledge_pro/knowledge_pro/settings.py
+++ knowledge_pro/knowledge_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k07l%97oql5^-k1o*avm7nj^y_7cv(uwbrp3mgb((kvd1l7doj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,6 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     (os.path.join(BASE_DIR, 'static'))
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/images/'
 
